6da6391cc86eb23c87ce36a86f1855d74f896713
LariCode <larissa.santosazevedo@hotmail.com>
2021-06-11 17:31:45 -0300

    feat: ensure Login calls validation with correct password

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -13,6 +13,7 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
   const [state, setState] = useState({
     isLoading: false,
     email: "",
+    password: "",
     emailError: "Campo obrigatório",
     passwordError: "Campo obrigatório",
     mainError: "",
@@ -21,6 +22,10 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
   useEffect(() => {
     validation.validate({ email: state.email });
   }, [state.email]);
+
+  useEffect(() => {
+    validation.validate({ password: state.password });
+  }, [state.password]);
 
   return (
     <div className={Styles.login}>
