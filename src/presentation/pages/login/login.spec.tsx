4613d2fe8facea79f27a9f19a4d8f4c9a0c874f7
LariCode <larissa.santosazevedo@hotmail.com>
2021-06-17 23:00:13 -0300

    test: ensure Login present error and hide spinner on authentication failure

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -5,9 +5,11 @@ import {
   RenderResult,
   fireEvent,
   cleanup,
+  waitFor,
 } from "@testing-library/react";
 import { ValidationStub, AuthenticationSpy } from "@/presentation/test";
 import Login from "./login";
+import { InvalidCredentialsError } from "@/domain/errors";
 
 type SutTypes = {
   sut: RenderResult;
@@ -149,12 +151,31 @@ describe("Login Component", () => {
     expect(authenticationSpy.callsCount).toBe(1);
   });
 
-  test("Should not call Authenticationif form is invalid", () => {
+  test("Should not call Authentication if form is invalid", () => {
     const validationError = faker.random.words();
     const { sut, authenticationSpy } = makeSut({ validationError });
 
     populateEmailField(sut);
     fireEvent.submit(sut.getByTestId("form"));
     expect(authenticationSpy.callsCount).toBe(0);
+  });
+
+  test("Should present error if Authentication fails", async () => {
+    const { sut, authenticationSpy } = makeSut();
+    const error = new InvalidCredentialsError();
+    jest
+      .spyOn(authenticationSpy, "auth")
+      .mockReturnValueOnce(Promise.reject(error));
+
+    simulateValidSubmit(sut);
+
+    const errorWrap = sut.getByTestId("error-wrap");
+
+    await waitFor(() => errorWrap);
+
+    const mainError = sut.getByTestId("main-error");
+    expect(mainError.textContent).toBe(error.message);
+
+    expect(errorWrap.childElementCount).toBe(1);
   });
 });
