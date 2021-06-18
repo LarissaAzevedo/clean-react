3b9ad764ab1682d4d4eb87d02300aac9102a9e76
LariCode <larissa.santosazevedo@hotmail.com>
2021-06-17 23:09:28 -0300

    test: ensure Login add accessToken to localStorage on success

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import faker from "faker";
+import "jest-localstorage-mock";
 import {
   render,
   RenderResult,
@@ -71,6 +72,10 @@ const simulateStatusForField = (
 };
 describe("Login Component", () => {
   afterEach(cleanup);
+
+  beforeEach(() => {
+    localStorage.clear();
+  });
 
   test("Should start with initial state", () => {
     const validationError = faker.random.words();
@@ -177,5 +182,18 @@ describe("Login Component", () => {
     expect(mainError.textContent).toBe(error.message);
 
     expect(errorWrap.childElementCount).toBe(1);
+  });
+
+  test("Should add accessToken to localStorage on success", async () => {
+    const { sut, authenticationSpy } = makeSut();
+
+    simulateValidSubmit(sut);
+
+    await waitFor(() => sut.getByTestId("form"));
+
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      "accessToken",
+      authenticationSpy.account.accessToken
+    );
   });
 });
