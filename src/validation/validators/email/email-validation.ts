7678c0a5198c7180f09588cd462e06d5946d6f6f
LariCode <larissa.santosazevedo@hotmail.com>
2021-07-01 00:47:22 -0300

    fix: ensure EmailValidation allows empty value

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -7,6 +7,6 @@ export class EmailValidation implements FieldValidation {
     const emailRegex =
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    return emailRegex.test(value) ? null : new InvalidFieldError();
+    return !value || emailRegex.test(value) ? null : new InvalidFieldError();
   }
 }
