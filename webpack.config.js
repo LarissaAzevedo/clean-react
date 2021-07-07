eb76b431424210cd9a43418f089c7dd95b083c95
LariCode <larissa.santosazevedo@hotmail.com>
2021-07-06 22:05:56 -0300

    feat: add env var to API URL

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
+const { DefinePlugin } = require("webpack");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/main/index.tsx",
   output: {
-    path: path.join(__dirname, 'public/js'),
+    path: path.join(__dirname, "public/js"),
     filename: "bundle.js",
     publicPath: "/public/js",
   },
@@ -15,23 +16,30 @@ module.exports = {
     },
   },
   module: {
-    rules: [{
-      test: /\.ts(x?)$/,
-      loader: 'ts-loader',
-      exclude: /node_modules/
-    }, {
-      test: /\.scss$/,
-      use: [{
-        loader: 'style-loader'
-      }, {
-        loader: 'css-loader',
-        options: {
-          modules: true
-        }
-      }, {
-        loader: 'sass-loader'
-      }]
-    }]
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
   },
   devServer: {
     contentBase: "./public",
@@ -42,5 +50,7 @@ module.exports = {
     react: "React",
     "react-dom": "ReactDOM",
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [new CleanWebpackPlugin(), new DefinePlugin({
+    'process.env.API_URL': 'http://fordevs.herokuapp.com/api'
+  })],
 };
