f1fe53f810f53c56cd4903acede753b0b185dfe6
LariCode <larissa.santosazevedo@hotmail.com>
2021-08-02 19:30:59 -0300

    test: ensure SignUp show valid status if password validation succeeds

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -81,4 +81,11 @@ describe('SignUp Component', () => {
     Helper.populateField(sut, 'email')
     Helper.testStatusForField(sut, 'email')
   })
+
+  test('Should show valid password if Validation succeeds', () => {
+    const { sut } = makeSut()
+
+    Helper.populateField(sut, 'password')
+    Helper.testStatusForField(sut, 'password')
+  })
 })
