f03007779287122597fd4c3ae07d8732867cba2b
LariCode <larissa.santosazevedo@hotmail.com>
2021-09-30 23:30:49 -0300

    feat: add SignUp Composition

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -6,5 +6,6 @@ import { Router } from '@/presentation/components'
 import '@/presentation/styles/global.scss'
 
 import { makeLogin } from './factories/pages/login/login-factory'
+import { makeSignUp } from './factories/pages/signup/signup-factory'
 
-ReactDOM.render(<Router makeLogin={makeLogin} />, document.getElementById('main'))
+ReactDOM.render(<Router makeLogin={makeLogin} makeSignUp={makeSignUp} />, document.getElementById('main'))
